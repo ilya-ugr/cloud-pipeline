ilya-ugr/cloud-pipeline

commit 9e856dad4de8a38f9d137108f6dcee761c7b7d80

pipectl: check available disk size when installing kube master/docker and transferring docker images

deploy/contents/install/app/install-utils.sh
@@ -34,6 +34,26 @@ function id_from_arn {
     echo $arn | cut -d/ -f2
 }
 
+# Will return amount of availble disk volume, mounted to the "$1" location
+# If "$1" is not specified - "/" will be used by default
+function get_available_disk {
+    local mount_path="${1:-/}"
+    df -m "$mount_path" | sed 1d | awk '{ print $4 }'
+}
+
+function check_enough_disk {
+    local min_disk="$1"
+    shift
+    local mount_locations="$@"
+
+    for location in $mount_locations; do
+        local location_free_volume="$(get_available_disk "$location")"
+        print_info "${location} has ${location_free_volume}MB free (required: ${min_disk}MB)"
+        (( "$location_free_volume" < "$min_disk" )) && return 1
+    done
+    return 0
+}
+
 function escape_comma_separated_values {
     local value="$1"
     IFS="," read -ra v_arr <<< "$value"
@@ -60,6 +80,8 @@ function run_preflight {
         print_err "Unsopported Linux distribution. Centos 7 and above shall be used"
         return 1
     fi
+
+    if [ -z ""]
     return 0
 }
 

deploy/contents/install/app/install-master.sh
@@ -16,6 +16,18 @@
 
 set -e
 
+# Check that we have enough disk space for the master compononents (root fs, docker, etcd)
+check_enough_disk "${CP_COMMON_ROOT_FS_MIN_DISK_MB:-20480}" \
+                  "/"
+
+check_enough_disk "${CP_KUBE_MASTER_DOCKER_MIN_DISK_MB:-102400}" \
+                  "$CP_KUBE_MASTER_DOCKER_PATH" "/var/lib/docker"
+
+check_enough_disk "${CP_KUBE_MASTER_ETCD_MIN_DISK_MB:-20480}" \
+                  "$CP_KUBE_MASTER_DOCKER_PATH" "/var/lib/etcd"
+
+
+
 # 1
 cat <<EOF >/etc/yum.repos.d/kubernetes.repo
 [kubernetes]
@@ -97,7 +109,7 @@ systemctl enable kubelet
 systemctl start docker
 systemctl start kubelet
 
-# TODO: here and further - implement a smarter approach to wait for the kube service to init
+# FIXME: here and further - implement a smarter approach to wait for the kube service to init
 sleep 10
 
 #9
@@ -126,7 +138,7 @@ if [ "$CP_KUBE_KEEP_KUBEADM_PROXIES" != "1" ]; then
 fi
 
 FLANNEL_CIDR=${FLANNEL_CIDR:-"10.244.0.0/16"}
-kubeadm init --pod-network-cidr="$FLANNEL_CIDR" --kubernetes-version v1.7.5 --skip-preflight-checks > $HOME/kubeadm_init.log
+kubeadm init --pod-network-cidr="10.244.0.0/16" --kubernetes-version v1.7.5 --skip-preflight-checks > $HOME/kubeadm_init.log
 
 sleep 30
 

deploy/contents/install/app/install.sh
@@ -532,6 +532,13 @@ fi
 if is_service_requested cp-docker-registry; then
     print_ok "[Starting Docker registry deployment]"
 
+    check_enough_disk "${CP_KUBE_MASTER_DOCKER_MIN_DISK_MB:-102400}" \
+                      "$CP_KUBE_MASTER_DOCKER_PATH" "/var/lib/docker"
+
+    if [ $? -ne 0 ]; then
+        print_err "!!! Docker images storage does not provide enough space (see above), subsequent images transfer (and overall installation) may fail !!!"
+    fi
+
     print_info "-> Deleting existing instance of Docker registry"
     delete_deployment_and_service   "cp-docker-registry" \
                                     "/opt/docker-registry" \

deploy/contents/install/install-config
@@ -1,6 +1,7 @@
 # Common
 CP_COMMON_SSL_SELF_SIGNED=false
 CP_COMMON_CERT_DIR=/opt/common/pki
+CP_COMMON_ROOT_FS_MIN_DISK_MB=20480
 
 
 # Kubernetes
@@ -8,6 +9,8 @@ CP_KUBE_EXTERNAL_HOST_TYPE=CP_CLOUD_INTERNAL_HOST
 CP_KUBE_INTERNAL_HOST=kubernetes.default.svc.cluster.local
 CP_KUBE_INTERNAL_PORT=443
 CP_KUBE_EXTERNAL_PORT=6443
+CP_KUBE_MASTER_DOCKER_MIN_DISK_MB=102400
+CP_KUBE_MASTER_ETCD_MIN_DISK_MB=20480
 CP_CLOUD_CREDENTIALS_LOCATION=/root/.cloud/credentials
 
 # cp-api-srv config

commit 82db16860e8ef01d2741a96cc35dcda4a7bd7bff

Fix install-utils.sh, broken by 9e856dad4de8a38f9d137108f6dcee761c7b7d80

deploy/contents/install/app/install-utils.sh
@@ -80,8 +80,6 @@ function run_preflight {
         print_err "Unsopported Linux distribution. Centos 7 and above shall be used"
         return 1
     fi
-
-    if [ -z ""]
     return 0
 }
 