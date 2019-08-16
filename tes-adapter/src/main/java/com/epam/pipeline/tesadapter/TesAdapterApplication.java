8113ce55680ec367d06e0324cf72d16179792a91
Ilya Ugryumov <ilya.ugryumov@mail.ru>
2019-08-16 18:29:11 +0300

    refactored MappingJackson2HttpMessageConverter

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/TesAdapterApplication.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/TesAdapterApplication.java
@@ -1,12 +1,13 @@
 package com.epam.pipeline.tesadapter;
 
+import com.epam.pipeline.tesadapter.configuration.CustomConverter;
 import com.epam.pipeline.tesadapter.configuration.TesSwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({TesSwaggerConfig.class})
+@Import({TesSwaggerConfig.class, CustomConverter.class})
 public class TesAdapterApplication {
     public static void main(String[] args) {
         SpringApplication.run(TesAdapterApplication.class, args);
