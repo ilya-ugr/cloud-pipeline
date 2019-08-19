1aac0101324c7386af7b992c2d06e4f80449ae0e
Ilya Ugryumov <ilya.ugryumov@mail.ru>
2019-08-19 17:52:57 +0300

    created TesTaskService, injected if to TesAdapterController

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/TesAdapterApplication.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/TesAdapterApplication.java
@@ -3,9 +3,11 @@ package com.epam.pipeline.tesadapter;
 import com.epam.pipeline.tesadapter.configuration.TesSwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@ComponentScan("com.epam.pipeline.tesadapter.service")
 @Import({TesSwaggerConfig.class})
 public class TesAdapterApplication {
     public static void main(String[] args) {
